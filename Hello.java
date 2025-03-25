61888fca29d58405563aeef58fd4bc9596d4194d
Dekel5030 <dekel5030@gmail.com>
2025-03-25 15:20:18 +0200

    Third push

--- Hello.java
+++ Hello.java
@@ -4,7 +4,7 @@ public class Hello
 	{
 		for (int i = 1; i <= 10; i++) 
 		{
-			System.out.println("Hello World second pull..." + i * 2);
+			System.out.println("Hello World three push..." + i * 2);
 		}
 	}
 }
